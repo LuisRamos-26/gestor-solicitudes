3de8f950f0ed8f9d4ea072aa047e5e01c6d3ad83
LuisRamos-26 <soporte.ulaes@gmail.com>
2026-07-11 22:45:31 -0600

    Proyecto DAA Unidad 2

---
+++ gestor-solicitudes/app/login/page.tsx
@@ -0,0 +1,8 @@
+export default function LotinPage() {
+  return (
+    <main style={{ padding: "2rem" }}>
+    <h1>Iniciar Sesion</h1>
+    <p>Pantalla de acceso para usuarios registrados</p>
+    </main>
+  );
+}
---
+++ gestor-solicitudes/app/solicitudes/page.tsx
@@ -0,0 +1,8 @@
+export default function SolicitudesPage() {
+  return (
+    <main style={{ padding: "2rem" }}>
+    <h1>Gestion de solicitudes</h1>
+    <p>Listado y administracion de solicitudes</p>
+    </main>
+  );
+}
